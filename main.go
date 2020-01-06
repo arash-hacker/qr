1236152f331b2d0f13b8daab2094903c2d6e3ffd
ArashMidos <arshiamidos@gmail.com>
2020-01-06 21:35:14 +0330

    masks

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"generator_polynomial_galois/gpg"
 	"qr/utils"
 	"qr_presenter/painter"
@@ -28,8 +27,6 @@ func main() {
 		utils.GetGroupBlock(v, e),
 		utils.GetECCWCount(v, e),
 	)
-	s:=GPG1.Serialize(utils.GetGroupBlock(v,e)["GROUP1"][0]+utils.GetGroupBlock(v,e)["GROUP2"][0],v)
-	fmt.Println(s)
-	fmt.Println("OOO",len(s))
-	painter.PaintV(v,10,s)
+	s := GPG1.Serialize(utils.GetGroupBlock(v, e)["GROUP1"][0]+utils.GetGroupBlock(v, e)["GROUP2"][0], v)
+	painter.PaintV(v, 10, s)
 }
